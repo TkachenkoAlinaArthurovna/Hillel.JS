5134d70e77b303b82c509cfcacff0fb2eb2c3036
TkachenkoAlinaArthurovna <tkachenkoalinaarthurovna@gmail.com>
2022-04-17 13:12:26 +0300

    Homework-3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,55 @@
+var operation
+while(true){
+  operation = prompt("Какую операцию вы хотите сделать?");
+  if(operation == '+' || operation == '-' || operation == '*' || operation == '/') break
+}
+var operand
+while(true){
+  operand = Number (prompt("Сколько операндов хотите использовать?"));
+  if(!isNaN(operand)) break
+}
+var result;
+for( var i = 1; i <= operand;i++){
+  var operandNumber
+  while(true){
+    operandNumber = Number (prompt(`Введите ${i}-е число?`));
+    if(!isNaN(operandNumber)) break
+  }
+  if (i == 1){
+    result = operandNumber
+    continue
+  }
+  switch(operation) {
+    case '+' : {
+      result = result + operandNumber
+      break;
+    }
+    case '-' : {
+      result = result - operandNumber
+      break;
+    }
+    case '*' : {
+      result = result * operandNumber
+      break;
+    }
+    case '/' : {
+      result = result / operandNumber
+      break;
+    }
+    
+  }
+}
+console.log (result);
+
+
+
+  
+
+
+
+
+
+
+  
+
 

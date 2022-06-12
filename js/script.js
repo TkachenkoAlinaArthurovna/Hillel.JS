5593e085e8fe540e38cc0f8a10a314886ae99ba5
TkachenkoAlinaArthurovna <tkachenkoalinaarthurovna@gmail.com>
2022-06-12 23:50:07 +0300

    Homework-18

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
+<<<<<<< Updated upstream
 
+=======
+const worker = {
+    name: '',
+    surname: '',
+    role: 'worker',
+    rate: 0,
+    days: 0,
+    getSalary: () => {
+        return this.rate * this.days
+    },
+    getSocialPackage: () => {},
+    timeOff: timeOff()
+}
+
+administrator(worker) {
+    role: 'administrator',
+    addCashier: () => {},
+    addConsultant: () => {},
+    getSocialPackage: () => {
+        if (worker.role == administrator) {
+            return [];
+        }
+
+    }
+}
+
+consultant(worker) {
+    role: 'consultant',
+    doProductInfo: () => {},
+    getSocialPackage: () => {
+        if (worker.role == consultant) {
+            return [];
+        }
+    }
+}
+
+cashier(consultant) {
+    role: 'cashier',
+    doSale: () => {},
+    getSocialPackage: () => {
+        if (worker.role == cashier) {
+            return null
+        }
+    }
+}
+
+const timeOff = () => {
+    var timeOffCount = 0;
+    return ({
+        add: (days) => timeOffCount + days,
+        remove: (days) => timeOffCount - days,
+        getTimeOffCount: () => timeOffCount
+    })
+}
+>>>>>>> Stashed changes
